11bad3331dc48c0892feaa8c0c11640e4ffa5154
adejumobiesther <64669672+adejumobiesther@users.noreply.github.com>
2020-06-06 15:20:06 +0100

    Update Computer guess game.py

--- Computer guess game.py
+++ Computer guess game.py
@@ -1,9 +1,11 @@
 import random
-computer_number = random.randint(0,20)
-player_guess = int(input("Enter a guess number between 0 and 20\n" ))
-if player_guess < computer_number:
-    print("The guess {}, is lower than the actual number by {}".format(player_guess, (computer_number-player_guess)))
-elif player_guess > computer_number:
-    print("The guess {}, is higher than the actual number by {}".format(player_guess, player_guess-computer_number))
-else:
-    print("You are a great guesser!")
+trial = int(input("how many times do you want to retry the guess_game?\n"))
+i,result = 0,0
+while i<trial:
+    computer_number = random.randint(0,20)
+    player_guess = int(input("Enter a guess number between 0 and 20\n" ))
+    print("correct!"if computer_number == player_guess else "wrong, try harder!")
+    result+=1 if computer_number == player_guess else result
+    i +=1
+
+print("You got {} guesses out of {} trials".format(result, trial))
