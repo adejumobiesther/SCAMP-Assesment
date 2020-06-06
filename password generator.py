3f25e117a5f17f4396c32d1fdda9ccbc281d830d
adejumobiesther <64669672+adejumobiesther@users.noreply.github.com>
2020-06-06 15:22:46 +0100

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,15 +1,14 @@
 import string
 import random
 def password_generator():
-    password_length = int(input("what should be the length of your password?\n"))
-    if password_length < 6:
-        raise ValueError("The password should be a minimum of 6 characters long!")
-    number_count = int(input("how many numbers, should the password contain?\n"))
-    letter_count = int(input("how many letters, should the password contain?\n"))
+    number_count = int(input("How many numbers, should the password contain?\n"))
+    letter_count = int(input("How many letters, should the password contain?\n"))
     numbers = string.digits + string.punctuation
     letters = string.ascii_letters
-    password = random.sample(letters, k = letter_count) + random.sample(numbers, k = number_count)
-    return " ".join(password)
+    pass_word = " ".join(random.sample(letters, k = letter_count) + random.sample(numbers, k = number_count))
+    if len(pass_word)< 6:
+        raise ValueError("The password should be a minimum of 6 characters long!")
+    return pass_word
 
 print(password_generator())
 
